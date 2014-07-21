602cf0c16a8e283aa391a670679e63fa4abba6dd
Morgan Ney <morganney@gmail.com>
2014-07-21 12:46:36 -0700

    add more to the es6 implementation

--- linkedlist.es6.js
+++ linkedlist.es6.js
@@ -1,17 +1,31 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
 class LinkedList {
-
-
   constructor() {
     super();
-    this.count = 0;
     this.head = { next: null };
+    this.count = 0;
+    this.OUT_OF_RANGE_ERR = 'Supplied index is out of range.'
+  }
+
+  find(n) {
+    var cur = this.head.next
+      , idx = 1
+    ;
+
+    if(n < 1 || n > size) cur = null;
+    else for(idx; idx < n; idx++) cur = cur.next;
+
+    return cur;
+
   }
 
   get size() {
     return this.count;
   }
 }
-
-var ll = new LinkedList();
-
-console.log(ll.size);
--- linkedlist.traceur.js
+++ linkedlist.traceur.js
@@ -1,14 +1,32 @@
+var Node = function Node(data) {
+  "use strict";
+  this.data = data;
+  this.next = null;
+};
+($traceurRuntime.createClass)(Node, {}, {});
 var LinkedList = function LinkedList() {
   "use strict";
   $traceurRuntime.superCall(this, $LinkedList.prototype, "constructor", []);
-  this.count = 0;
   this.head = {next: null};
+  this.count = 0;
+  this.OUT_OF_RANGE_ERR = 'Supplied index is out of range.';
 };
 var $LinkedList = LinkedList;
-($traceurRuntime.createClass)(LinkedList, {get size() {
+($traceurRuntime.createClass)(LinkedList, {
+  find: function(n) {
+    "use strict";
+    var cur = this.head.next,
+        idx = 1;
+    ;
+    if (n < 1 || n > size)
+      cur = null;
+    else
+      for (idx; idx < n; idx++)
+        cur = cur.next;
+    return cur;
+  },
+  get size() {
     "use strict";
     return this.count;
-  }}, {});
-var ll = new LinkedList();
-console.log(ll.size);
-console.log(ll.head);
+  }
+}, {});
